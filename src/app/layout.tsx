b74478be8f8aecc909e60a23d5ce8a32bdf15def
PRIYANSHU PRASAD GUPTA <100031428+priyanshu2k3@users.noreply.github.com>
2024-03-28 01:17:54 +0530

    fixed the tittle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head><title>PPG portfolio</title></head>
       <body className={inter.className}>{children}</body>
     </html>
   );
